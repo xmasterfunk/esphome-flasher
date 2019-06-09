c967cf2b794c640cc7c73e64b0946247f187b8b5
Otto Winter <otto@otto-winter.com>
2019-06-09 15:26:28 +0200

    Bump version to 1.1.0

--- esphomeflasher/const.py
+++ esphomeflasher/const.py
@@ -1,11 +1,11 @@
 import re
 
-__version__ = "1.0.2"
+__version__ = "1.1.0"
 
 ESP32_DEFAULT_OTA_DATA = 'https://raw.githubusercontent.com/espressif/arduino-esp32/1.0.0/tools/partitions/boot_app0.bin'
 ESP32_DEFAULT_BOOTLOADER_FORMAT = 'https://raw.githubusercontent.com/espressif/arduino-esp32/' \
                                   '1.0.0/tools/sdk/bin/bootloader_$FLASH_MODE$_$FLASH_FREQ$.bin'
-ESP32_DEFAULT_PARTITIONS = 'https://raw.githubusercontent.com/OttoWinter/esphomeflasher/master/partitions.bin'
+ESP32_DEFAULT_PARTITIONS = 'https://raw.githubusercontent.com/esphome/esphomeflasher/master/partitions.bin'
 
 # https://stackoverflow.com/a/3809435/8924614
 HTTP_REGEX = re.compile(r'https?://(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&/=]*)')
